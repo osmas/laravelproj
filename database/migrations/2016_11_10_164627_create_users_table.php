c2ca8704f7daa4b2f59cc3495fc8f946cc7dd24c
osmas <osmas@hotmail.co.uk>
2016-11-10 17:22:18 +0000

    users migration

--- database/migrations/2016_11_10_164627_create_users_table.php
+++ database/migrations/2016_11_10_164627_create_users_table.php
@@ -15,10 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->username()->unique;
+            $table->email()->unique;
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('users');
     }
 }
